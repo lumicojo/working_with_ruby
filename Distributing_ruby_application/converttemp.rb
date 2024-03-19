117be0ccc7ce72ace5d8e23262786a7c87bc565f
+ <lmcjcr@gmail.com>
2024-03-19 17:28:21 -0400

    working self, minitest

---
+++ Distributing_ruby_application/converttemp.rb
@@ -0,0 +1,9 @@
+print "Enter a fahrenheit temperature: "
+temp = gets.chomp
+temp = temp.to_i
+puts (5.0 / 9.0) * (temp - 32.0)
+# was gest a test
+
+
+# temp = ARGV[0].to_i
+# puts (5.0 / 9.0) * (temp - 32.0)
